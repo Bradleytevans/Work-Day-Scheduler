c55016d27486c5fe48eb491d438c85f9da719a12
z0mbiebrad <Bradleyt.evans@gmail.com>
2021-09-27 07:26:01 -0400

    fixed display issue

--- Assets/script.js
+++ Assets/script.js
@@ -10,16 +10,16 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
     //calls on the local storage for each hour so when the page refreshes your activity is saved
-    $("#am-8 .description").val(localStorage.getItem("am-8"));
-    $("#am-9 .description").val(localStorage.getItem("am-9"));
-    $("#am-10 .description").val(localStorage.getItem("am-10"));
-    $("#am-11 .description").val(localStorage.getItem("am-11"));
-    $("#pm-12 .description").val(localStorage.getItem("pm-12"));
-    $("#pm-1 .description").val(localStorage.getItem("pm-1"));
-    $("#pm-2 .description").val(localStorage.getItem("pm-2"));
-    $("#pm-3 .description").val(localStorage.getItem("pm-3"));
-    $("#pm-4 .description").val(localStorage.getItem("pm-4"));
-    $("#pm-5 .description").val(localStorage.getItem("pm-5"));
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("hour-17 .description").val(localStorage.getItem("hour-17"));
 
     function hourTracker() {
         //using moment.js again to establish the current hour
@@ -28,6 +28,7 @@ $(document).ready(function () {
         $(".time-block").each(function () {
             //using parseInt() to establish the block hour by selecting the id of each hour, using split to remove the am and pm
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            console.log( blockHour, currentHour)
             //checks to see what css theme to apply based on time
             if (blockHour < currentHour) {
                 $(this).addClass("past");
@@ -46,5 +47,6 @@ $(document).ready(function () {
             }
         })
     }
-    hourTracker(); //repeats the function s
+    hourTracker(); //repeats the function 
+    var interval = setInterval(hourTracker, 15000);
 })
